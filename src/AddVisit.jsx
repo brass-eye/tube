4ec912f7cbf633a554b49aa68df5ffe18078ecb3
aiden <aidenscott12@googlemail.com>
2017-05-03 20:24:07 +0100

    Support loading stations from api

--- src/AddVisit.jsx
+++ src/AddVisit.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import TubeData from './tfl-tube-data.json'
 
 export default class AddVisit extends React.PureComponent {
 	constructor() {
 		super();
 
-		this.state = {
-			value: Object.keys(TubeData.stations)[0] 
-		};
+		this.state = {stations: []};
+	}
+
+	componentDidMount(){
+    fetch("http://localhost:4567/stations", {method: 'get'})
+      .then(response => {
+          return response.json()
+        })
+      .then(json => {
+      	this.setState({
+      		stations: json,
+      		value: "WAT"
+      	});
+      })
 	}
 
 
 	render() {
+		debugger;
 		return (
 			<form onSubmit={this.props.onSubmit}>
 				<select onChange={this.props.onChange}>
 					{
-						Object.keys(TubeData.stations).map( (stop, key) => {
+						this.state.stations.map( (stop, key) => {
 							 return <option key={key}
-							 								value={stop}>
-							 								{TubeData.stations[stop]}
+							 								value={stop.code}>
+							 								{stop.name}
 							 				</option>;
 						})
 					}
